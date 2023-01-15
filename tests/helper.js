dffc70d35864d684e89acf14267f236980e63c45
Gallifrey <sird.mallika@gmail.com>
2023-01-15 14:51:28 +0530

    TAT and WAT work

--- tests/helper.js
+++ tests/helper.js
@@ -5,8 +5,8 @@
 Update these every time new pictures and stuff are added
 */
 const num_ppdt = 1;
-const num_tat = 1;
-const num_wat = 1;
+const num_tat = 11;
+const num_wat = 60;
 const num_srt = 1;
 const ppdt_dir = '../resources/ppdt/';
 const tat_dir = '../resources/tat/';
@@ -33,9 +33,8 @@ window.matchMedia('(display-mode: fullscreen)').addEventListener('change', ({ ma
 });
 
 // Get a random image from directory
-function getImg(dir) {
-    picture_no = Math.floor(Math.random() * (num_ppdt-1));
-    return dir+picture_no+'.jpg';
+function getImg(dir, idx) {
+    return dir+idx+'.jpg';
 }
 
 // Display an image on the screen
@@ -54,6 +53,15 @@ function playAlert() {
     location.reload();
 }
 
+// Get tat images
+function getRandomNos(max_num,samplesize) {
+    var x = [];
+    for (let i = 0; i < max_num; i ++) {
+        x.push(i)
+    }
+    return _.sample(x, samplesize);
+}
+
 /*
 ===============================================================
                         PPDT Test Code
@@ -62,8 +70,45 @@ function playAlert() {
 $('#start-ppdt').click(function(e) {
     $('#ppdt-rules').addClass('invisible');
     $('#ppdt-test').removeClass('invisible');
-    setTimeout(function() {displayImg(getImg(ppdt_dir),$('#ppdt-test'))}, 5000);
+    idx = Math.floor(Math.random() * (num_ppdt-1));
+    setTimeout(function() {displayImg(getImg(ppdt_dir,idx),$('#ppdt-test'))}, 5000);
     setTimeout(function() {removeImage($('img'))}, 5000+30000);
     setTimeout(function() {$('#instruction').removeClass('invisible')}, 5000+30000+30000);
     setTimeout(playAlert, 5000+30000+30000+4*60*1000);
+});
+
+
+/*
+===============================================================
+                        TAT Test Code
+===============================================================
+*/
+$('#start-tat').click(function(e) {
+    $('#tat-rules').addClass('invisible');
+    $('#tat-test').removeClass('invisible');
+    imgs = getRandomNos(num_tat,11);
+    imgs.push('blank');
+    for (let i = 0; i < imgs.length; i++) {
+        setTimeout(function() {$('#instruction').addClass('invisible');displayImg(getImg(tat_dir,imgs[i]),$('#tat-test'))}, (5*60*1000)*i+5000);
+        setTimeout(function() {removeImage($('img'));$('#instruction').removeClass('invisible')}, (5*60*1000)*i+30000+5000);
+    }
+    setTimeout(playAlert, 5*60*1000*12+5000);
+});
+
+
+/*
+===============================================================
+                        WAT Test Code
+===============================================================
+*/
+$('#start-wat').click(function(e) {
+    $('#wat-rules').addClass('invisible');
+    $('#wat-test').removeClass('invisible');
+    wrds = getRandomNos(num_wat,60);
+    for (let i = 0; i < wrds.length; i++) {
+        setTimeout(function() {
+            $('#instruction').html('<h5>'+wat_words[wrds[i]]+'</h5>')
+        }, (15*1000)*i+5000);
+    }
+    setTimeout(playAlert, 15*1000*60+5000);
 });
